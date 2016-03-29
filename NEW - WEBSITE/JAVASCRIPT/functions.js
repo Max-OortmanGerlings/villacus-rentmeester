05b2d45cce51f11061d57a45bb3f2634f9a909c3
Max-OortmanGerlings <maxoortmangerlings@gmail.com>
2016-03-29 12:11:44 +0200

    code chane

---
+++ NEW - WEBSITE/JAVASCRIPT/functions.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('#Link-BeheerLandelijkGebied').click(function(){
+    $('#BeheerLandelijkGebied').css('display','block');
+    $("html, body").animate({ scrollTop: "0px" });
+    return false;
+  });
+
+  $('#Link-RuimtelijkBestuursrecht').click(function(){
+    $('#RuimtelijkBestuursrecht').css('display','block');
+    $("html, body").animate({ scrollTop: "0px" });
+    return false;
+  });
+
+  $('#Link-Ontwikkeling').click(function(){
+    $('#Ontwikkeling').css('display','block');
+    $("html, body").animate({ scrollTop: "0px" });
+    return false;
+  });
+
+  $('#Link-TaxatieLandelijk').click(function(){
+    $('#TaxatieLandelijk').css('display','block');
+    $("html, body").animate({ scrollTop: "0px" });
+    return false;
+  });
+
+  $('#Link-Bedrijf').click(function(){
+    $('#Bedrijf').css('display','block');
+    $("html, body").animate({ scrollTop: "0px" });
+    return false;
+  });
+
+  $('.close-modal').click(function(){
+    $('.modal').css('display','none');
+    return false;
+  });
+});
